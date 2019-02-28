9d0e3da8f56818017e218a3fd283e5aad1f2608a
okno <paul.delafosse@protonmail.com>
2019-02-28 01:36:43 +0100

    readme

--- README.md
+++ README.md
@@ -1,54 +1,23 @@
 # Compil TP 1
 
 # Prérequis
-- rustc 1.32.0-nightly (6bfb46e4a 2018-11-26)
+- rustc 1.34.0-nightly
 - cargo
-## Usage
 
 ### build
-```cargo build```
+
+```cargo build --release```
 
 ### run
-- with cargo
-```cargo run ${path_to_ast_json_file}```
-- with bin
-```./target/debug/jsparser ${path_to_ast_json_file}```
 
-## Literals :
-    - StringLiteral
-    - NumericLiteral
-    - BooleanLiteral)
-    - NullLiteral
-    - Infinity
+The binary file is located in `target/release`, run it like so : ```./rjsc ${flag} ${args}```
+If you are lost :`rjsc --help`. 
+
 
 ## Todo :
 
 - implement loc
-- implement the other control flow expression
-- implement interpretter for function and object
-- add unit test for operator overloading
-- add it test for interpretter
-- add cli argument for pretty printer/interpret mode
-- add repl mode
-- add semantic documentation
+- add dict generics in databox.h
+- implement object
+- implement nested function
 
-## To fix :
-
-- remote dirty copy with the Cell/RefCell type or a custom Box<> implementation
-- implement a custom deserializer for JSLiteral or flatten the Expression struct
-
-```
-#include "c_datalib/databox.h"
-#include <stdio.h>
-
-void main() {
-    databox x = {.data.num = 0};
-    double *ptx = &(x.data.num);
-    printf("%f", ptx);
-    while (*ptx < 10) {
-        *ptx+=1;
-        printf("%f\n", *ptx);
-    }
-
-} 
-```
--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::path::Path;
 use std::fs::File;
 use std::io::Write;
+use std::io;
 
 /// This just write the c_datalib into static str constant before compilation
 /// so that the c_datalib is available at run time
@@ -34,28 +35,31 @@ fn main() {
     let dest_path = Path::new(&out_dir).join("c_lib.rs");
     let mut f = File::create(&dest_path).unwrap();
 
+    create_temp_lib(databox_c, databox_h, print_c, print_h, &mut f)
+        .expect("Error while writing c_libs");
+}
+
+fn create_temp_lib(databox_c: String, databox_h: String, print_c: String, print_h: String, f: &mut File)
+ -> Result<(), io::Error> {
     let databox_c_const = b"pub const DATABOX_C: &'static str = ";
     let databox_h_const = b"pub const DATABOX_H: &'static str = ";
     let print_c_const = b"pub const PRINT_C: &'static str = ";
     let print_h_const = b"pub const PRINT_H: &'static str = ";
-
-    f.write_all(databox_c_const).unwrap();
-    f.write_all(b"\"");
-    f.write_all(databox_c.as_bytes()).unwrap();
-    f.write_all(b"\";");
-
-    f.write_all(databox_h_const).unwrap();
-    f.write_all(b"\"");
-    f.write_all(databox_h.as_bytes()).unwrap();
-    f.write_all(b"\";");
-
-    f.write_all(print_c_const).unwrap();
-    f.write_all(b"\"");
-    f.write_all(print_c.as_bytes()).unwrap();
-    f.write_all(b"\";");
-
-    f.write_all(print_h_const).unwrap();
-    f.write_all(b"\"");
-    f.write_all(print_h.as_bytes()).unwrap();
-    f.write_all(b"\";");
+    f.write_all(databox_c_const)?;
+    f.write_all(b"\"")?;
+    f.write_all(databox_c.as_bytes())?;
+    f.write_all(b"\";")?;
+    f.write_all(databox_h_const)?;
+    f.write_all(b"\"")?;
+    f.write_all(databox_h.as_bytes())?;
+    f.write_all(b"\";")?;
+    f.write_all(print_c_const)?;
+    f.write_all(b"\"")?;
+    f.write_all(print_c.as_bytes())?;
+    f.write_all(b"\";")?;
+    f.write_all(print_h_const)?;
+    f.write_all(b"\"")?;
+    f.write_all(print_h.as_bytes())?;
+    f.write_all(b"\";")?;
+    Ok(())
 }
